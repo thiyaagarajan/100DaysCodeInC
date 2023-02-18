144df58f9456c288506196efa15556328aa5e65c
Arun-Thiyaagarajan <isronatking963.at@gmail.com>
2023-02-18 14:23:42 +0530

    Day 15 & 16 Code Added

---
+++ Day 16/Day16_Attendence.c
@@ -0,0 +1,77 @@
+/*
+#Student Attedence Record - I (LEET CODE)
+
+You are given a string s representing an attendance record for a student where each character signifies whether the student was absent, late, or present on that day. The record only contains the following three characters:
+
+'A': Absent.
+'L': Late.
+'P': Present.
+The student is eligible for an attendance award if they meet both of the following criteria:
+
+The student was absent ('A') for strictly fewer than 2 days total.
+The student was never late ('L') for 3 or more consecutive days.
+Return true if the student is eligible for an attendance award, or false otherwise.
+
+Example 1:
+
+Input: s = "PPALLP"
+Output: true
+Explanation: The student has fewer than 2 absences and was never late 3 or more consecutive days.
+Example 2:
+
+Input: s = "PPALLL"
+Output: false
+Explanation: The student was late 3 consecutive days in the last 3 days, so is not eligible for the award.
+
+Constraints:
+
+1 <= s.length <= 1000
+s[i] is either 'A', 'L', or 'P'.
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+int checkRecord(char *s){
+
+    int i = 0, absent = 0, late = 0;
+
+    while (i < strlen(s))
+    {
+        if (s[i] == 'A' || s[i] == 'a')
+        {
+            absent++;
+            if (absent > 1)
+                return 0;
+            late = 0;
+        }
+        else if (s[i] == 'L' || s[i] == 'l')
+        {
+            late++;
+            if (late > 2)
+                return 0;
+        }
+        else
+            late = 0;
+        i++;
+    }
+    return 1;
+}
+
+int main()
+{
+    char attedence[50];
+    int award;
+
+    printf("\nEnter The Attedence Record String: ");
+    scanf("%s", attedence);
+
+    award = checkRecord(attedence);
+
+    if(award == 1)
+        printf("\nYeah! The Student is Eligible for Attendence Award.\n");
+    else
+        printf("\nOops! The Student is Not Eligible for Attendence Award.\n");
+
+    return 0;
+}
